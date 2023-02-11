d3242a3e599a4d85d84cbf70136383c78d386f59
ACT1GMR <act1gmr@googlemail.com>
2023-02-11 15:48:41 +0330

    - changing url of service

--- src/features/list/API.js
+++ src/features/list/API.js
@@ -1,4 +1,4 @@
 export function fetchList(amount = 1) {
-  return fetch('http://localhost:3000/aparat?url=http://api.aparat.com/fa/v1/video/video/mostViewedVideos')
+  return fetch('http://api.aparat.com/fa/v1/video/video/mostViewedVideos')
     .then(response => response.json())
 }
